daa5acf1d80e2b3b258f6d471bc9ea4c0f48adb9
Thomas Maloney <thomastnf@live.com>
2015-09-07 16:36:04 -0500

    added project

---
+++ NAudioTestingApp/NAudioTestingApp/Form1.Designer.cs
@@ -0,0 +1,66 @@
+﻿namespace NAudioTestingApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnPlay = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnPlay
+            // 
+            this.btnPlay.AutoSize = true;
+            this.btnPlay.Location = new System.Drawing.Point(514, 165);
+            this.btnPlay.Name = "btnPlay";
+            this.btnPlay.Size = new System.Drawing.Size(86, 30);
+            this.btnPlay.TabIndex = 0;
+            this.btnPlay.Text = "Play Note";
+            this.btnPlay.UseVisualStyleBackColor = true;
+            this.btnPlay.Click += new System.EventHandler(this.btnPlay_Click);
+            this.btnPlay.MouseDown += new System.Windows.Forms.MouseEventHandler(this.btnPlay_MouseDown);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(705, 404);
+            this.Controls.Add(this.btnPlay);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Form1_KeyPress);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnPlay;
+    }
+}
+
---
+++ NAudioTestingApp/NAudioTestingApp/Form1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using NAudio.Midi;
+using System.Threading;
+
+namespace NAudioTestingApp
+{
+    
+
+    public partial class Form1 : Form
+    {
+        Music mus;
+
+        Thread oThread;
+        Thread tThread;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            mus = new Music();
+            oThread = new Thread(new ThreadStart(mus.Play));
+            tThread = new Thread(new ThreadStart(mus.PlayNotes));
+
+            oThread.Start();
+            tThread.Start();
+
+            while (!oThread.IsAlive) ;
+            while (!tThread.IsAlive) ;
+
+            
+
+        }
+
+
+        private void btnPlay_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            oThread.Interrupt();
+            tThread.Interrupt();
+
+            oThread.Abort();
+            tThread.Abort();
+        }
+
+        private void Form1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == (char)Keys.P)
+            {
+                GlobVars.playSound = !GlobVars.playSound;
+            }
+        }
+
+        private void btnPlay_MouseDown(object sender, MouseEventArgs e)
+        {
+            GlobVars.isPressed = true;
+        }
+    }
+
+    public static class GlobVars
+    {
+        public static bool isPressed = false;
+        public static bool playSound = true;
+
+    }
+}
